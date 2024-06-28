77570866889d660b10091f0a44955c46e8c8bc36
Tiffany <tbcasper16@gmail.com>
2024-06-28 15:26:46 -0500

    task two; resolve function and test query. encountering a syntax error in graphiql but moving on for now

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,4 +1,20 @@
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = require('graphql');
+const lodash = require('lodash');
+
+const tasks = [
+    {
+        id: '1',
+        title: 'Create your first webpage',
+        weight: 1,
+        description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'
+    },
+    {
+        id: '2',
+        title: 'Structure your webpage',
+        weight: 1,
+        description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'
+    }
+];
 
 const TaskType = new GraphQLObjectType({
     name: 'Task',
@@ -17,11 +33,10 @@ const RootQuery = new GraphQLObjectType({
             type: TaskType,
             args: { id: { type: GraphQLString } },
             resolve(parent, args) {
+                return _.find(tasks, { id: args.id });
             }
         }
     }
 });
 
-module.exports = new GraphQLSchema({
-    query: RootQuery
-});
+module.exports = new GraphQLSchema({ query: RootQuery });

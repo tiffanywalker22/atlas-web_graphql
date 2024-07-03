0b311505a08a92f9a2901b4dff72d6b491766fb4
tiffany <tbcasper16@gmail.com>
2024-07-02 19:32:32 -0500

    got react running and updated app

--- GraphQL_API/client/src/App.js
+++ GraphQL_API/client/src/App.js
@@ -1,28 +1,26 @@
-/*import ApolloClient from 'apollo-boost';
-/*
-...
-*/
-// components
+import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 import TaskList from './components/TaskList';
 import AddTask from './components/AddTask';
 import AddProject from './components/AddProject';
-// apollo client setup
-/*
-...
-*/
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
+
 function App() {
-  return ( <
-    div id = "main" >
-    <
-    div id = "bg" > < /div> <
-    h1 > Holberton school tasks < /h1> <
-    TaskList / >
-    <
-    AddProject / >
-    <
-    AddTask / >
-    <
-    /div>
+  return (
+  <ApolloProvider client = { client }>
+    <div id = "main">
+    <div id = "bg" ></div>
+    <h1>Holberton school tasks</h1>
+    <TaskList />
+    <AddProject />
+    <AddTask />
+    </div>
+  </ApolloProvider>
   );
 }
 

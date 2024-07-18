55723066f5184cb33a045536be5a6c6da493f033
tiffany <tbcasper16@gmail.com>
2024-07-18 16:32:25 -0500

    making a few quick changes

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,7 +1,38 @@
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLID, GraphQLList, GraphQLNonNull } = require('graphql');
-const lodash = require('lodash');
 const Project = require('../models/project');
 const Task = require('../models/task');
+
+const projects = [
+    {
+        id: '1',
+        title: 'Advanced HTML',
+        weight: 1,
+        description: 'Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!'
+    },
+    {
+        id: '2',
+        title: 'Bootstrap',
+        weight: 1,
+        description: 'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.'
+    }
+];
+const tasks = [
+    {
+        id: '1',
+        title: 'Create your first webpage',
+        weight: 1,
+        description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)',
+        projectId: '1'
+    },
+    {
+        id: '2',
+        title: 'Structure your webpage',
+        weight: 1,
+        description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order',
+        projectId: '1'
+    }
+];
+
 
 const TaskType = new GraphQLObjectType({
     name: 'Task',
@@ -13,7 +44,7 @@ const TaskType = new GraphQLObjectType({
         project: {
             type: ProjectType,
             resolve(parent, args) {
-                return Project.findById(parent.projectId);
+                return (Project.findById(parent.projectId));
             }
         }
     })
@@ -29,7 +60,7 @@ const ProjectType = new GraphQLObjectType({
         tasks: {
             type: new GraphQLList(TaskType),
             resolve(parent, args) {
-                return Task.find({ projectId: parent.id });
+                return (Task.find({ projectId: parent.id }));
             }
         }
     })
@@ -42,26 +73,26 @@ const RootQuery = new GraphQLObjectType({
             type: TaskType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return Task.findById(args.id);
+                return (Task.findById(args.id));
             }
         },
         project: {
             type: ProjectType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return Project.findById(args.id);
+                return (Project.findById(args.id));
             }
         },
         tasks: {
             type: new GraphQLList(TaskType),
             resolve(parent, args) {
-                return Task.find({});
+                return (Task.find({}));
             }
         },
         projects: {
             type: new GraphQLList(ProjectType),
             resolve(parent, args) {
-                return Project.find({});
+                return (Project.find({}));
             }
         }
     }
@@ -83,7 +114,7 @@ const Mutation = new GraphQLObjectType({
                     weight: args.weight,
                     description: args.description
                 });
-                return project.save();
+                return (project.save());
             }
         },
         addTask: {
@@ -101,7 +132,7 @@ const Mutation = new GraphQLObjectType({
                     description: args.description,
                     projectId: args.projectId
                 });
-                return task.save();
+                return (task.save());
             }
         }
     }
